177fa1b09937c40276762d55011d6f8803773ca5
tom <tom@dnomaid.co.uk>
2017-12-22 17:34:18 +0000

    Change prompt symbol thing

--- cmd.py
+++ cmd.py
@@ -6,17 +6,19 @@ import os
 print('PyCmd\n')
 
 def prompt():
-    cmd = raw_input('$ ')
+    cmd = raw_input('$> ')
 
     if (cmd == 'exit'):
         quit()
 
     if (not cmd):
         print('Might be a nice idea if you actually type something mate')
+        # raise Exception('No Command Entered')
 
     try:
         os.system(cmd)
     except:
+        # raise Exception('Error Running Command')
         pass
 
     prompt()
