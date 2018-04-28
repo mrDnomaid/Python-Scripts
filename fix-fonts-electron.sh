42d4f36868292bc7de901d1f4b8cf8869d3d5b4d
Tom <tom@dnomaid.co.uk>
2018-04-28 15:28:26 +0100

    Hold the packages from being upgraded

--- fix-fonts-electron.sh
+++ fix-fonts-electron.sh
@@ -3,15 +3,22 @@ echo "Downloading DEB files from Ubuntu pool"
 wget http://security.ubuntu.com/ubuntu/pool/main/f/freetype/libfreetype6_2.8-0.2ubuntu2.1_amd64.deb
 wget http://security.ubuntu.com/ubuntu/pool/main/f/freetype/libfreetype6_2.8-0.2ubuntu2.1_i386.deb
 
-echo 'Force installing them over older/newer versions'
+echo "Force installing them over older/newer versions"
 sudo dpkg --force-all -i libfreetype6_2.8-0.2ubuntu2.1_i386.deb
 sudo dpkg --force-all -i libfreetype6_2.8-0.2ubuntu2.1_amd64.deb
+
+echo "Stopping the packages from being updated using apt (holding)"
+sudo apt-mark hold libfreetype6:i386
+sudo apt-mark hold libfreetype6:amd64
 
 echo "Deleting DEB files now they're installed"
 rm libfreetype6_2.8-0.2ubuntu2.1_i386.deb
 rm libfreetype6_2.8-0.2ubuntu2.1_amd64.deb
 
+
 echo "Done!"
 echo " "
 echo "Thanks to this reddit comment for the fix that I modified a bit:"
 echo "https://www.reddit.com/r/discordapp/comments/7w9c5f/fonts_are_really_screwed_up_on_linux/du5sjxz/"
+echo " "
+echo "Reboot, and hopefully this fixed your font problem!"
