d1c374651ba8299650594159673fee108a5b58eb
Tom <tom@dnomaid.co.uk>
2018-04-28 15:18:54 +0100

    Delete files after install and add verbosity

--- fix-fonts-electron.sh
+++ fix-fonts-electron.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+echo 'Downloading DEB files from Ubuntu pool'
 wget http://security.ubuntu.com/ubuntu/pool/main/f/freetype/libfreetype6_2.8-0.2ubuntu2.1_amd64.deb
 wget http://security.ubuntu.com/ubuntu/pool/main/f/freetype/libfreetype6_2.8-0.2ubuntu2.1_i386.deb
 
+echo 'Force installing them over older/newer versions'
 sudo dpkg --force-all -i libfreetype6_2.8-0.2ubuntu2.1_i386.deb
 sudo dpgk --force-all -i libfreetype6_2.8-0.2ubuntu2.1_amd64.deb
 
-# Thanks to this reddit comment for the fix: https://www.reddit.com/r/discordapp/comments/7w9c5f/fonts_are_really_screwed_up_on_linux/du5sjxz/
+echo 'Deleting DEB files now they\'re installed'
+rm libfreetype6_2.8-0.2ubuntu2.1_i386.deb
+rm libfreetype6_2.8-0.2ubuntu2.1_amd64.deb
+
+echo 'Done!'
+echo ''
+echo 'Thanks to this reddit comment for the fix that I modified a bit:'
+echo 'https://www.reddit.com/r/discordapp/comments/7w9c5f/fonts_are_really_screwed_up_on_linux/du5sjxz/'
