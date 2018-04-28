4912867f2a30f8c79d34929729d83fae0a284845
Tom <tom@dnomaid.co.uk>
2018-04-28 15:16:59 +0100

    Add shebang and comment

--- fix-fonts-electron.sh
+++ fix-fonts-electron.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
 wget http://security.ubuntu.com/ubuntu/pool/main/f/freetype/libfreetype6_2.8-0.2ubuntu2.1_amd64.deb
 wget http://security.ubuntu.com/ubuntu/pool/main/f/freetype/libfreetype6_2.8-0.2ubuntu2.1_i386.deb
 
 sudo dpkg --force-all -i libfreetype6_2.8-0.2ubuntu2.1_i386.deb
 sudo dpgk --force-all -i libfreetype6_2.8-0.2ubuntu2.1_amd64.deb
 
+# Thanks to this reddit comment for the fix: https://www.reddit.com/r/discordapp/comments/7w9c5f/fonts_are_really_screwed_up_on_linux/du5sjxz/
